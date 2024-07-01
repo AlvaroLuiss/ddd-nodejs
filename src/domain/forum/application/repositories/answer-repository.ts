86594f3cb760916576a6de1d5c46d186560b51ab
alvaroluiss <alvaro.luiscosta@hotmail.com>
2024-07-01 14:15:19 -0300

    feat: fetch-question-answers

--- src/domain/forum/application/repositories/answer-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Answer } from '../../enterprise/entities/answer'
-
-export interface AnswerRepository {
-  findById(id: string): Promise<Answer | null>
-  create(answer: Answer): Promise<void>
-  save(answer: Answer): Promise<void>
-  delete(answer: Answer): Promise<void>
-}
