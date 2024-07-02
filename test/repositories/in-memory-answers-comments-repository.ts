7cab9fdd1840573cb83d24bd72e1e82d88e72617
alvaroluiss <alvaro.luiscosta@hotmail.com>
2024-07-01 22:35:42 -0300

    feat: delete-answer-question use case

--- test/repositories/in-memory-answers-comments-repository.ts
+++ test/repositories/in-memory-answers-comments-repository.ts
@@ -2,10 +2,28 @@ import { AnswersCommentsRepository } from '@/domain/forum/application/repositori
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
 export class InMemoryAnswerCommentsRepository implements AnswersCommentsRepository {
+  delte(answerComment: AnswerComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
   public items: AnswerComment[] = []
 
- 
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === answerComment.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
